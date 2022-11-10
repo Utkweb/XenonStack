b7a69afe4ee11ea298c91beb1e511fa0fcafcaf0
Utkweb <us196000@gmail.com>
2022-11-10 06:00:12 +0530

    ...

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+const api_config={
+    port:5000,
+    dbUrl:"mongodb+srv://UtkarshS:7236989916@cluster0.o9fwt.mongodb.net/XenonStack?retryWrites=true&w=majority"
+
+}
+
+module.exports = api_config;
---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const userRouter = require("./router/userRouter");
+const contactRouter = require("./router/contactRouter");
+const cors = require("cors");
+const api_config = require("./config");
+
+const app = express();
+const port = api_config.port;
+
+// for parsing request data
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+
+//address
+app.use("/user", userRouter);
+app.use("/contact", contactRouter);
+
+app.get("/home", (req, res) => {
+  console.log("request at home");
+  res.send("the wait is over!");
+});
+
+app.listen(port, () => {
+  console.log("server started");
+});
